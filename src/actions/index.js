c3dab2b00370ab8c5b41feb597382ba43066b0e4
Robert Leap <robertleap@gmail.com>
2019-06-23 20:19:14 -0700

    checking in updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import streams from "../apis/streams";
+import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -7,7 +8,7 @@ import {
   FETCH_STREAM,
   DELETE_STREAM,
   EDIT_STREAM
-} from "./types";
+} from './types';
 
 export const signIn = userId => {
   return {
@@ -22,14 +23,17 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
-  const response = await streams.get("/streams");
+  const response = await streams.get('/streams');
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
@@ -41,13 +45,15 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push('/');
 };
